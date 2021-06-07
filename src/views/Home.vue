00270b3296dc4a6570fef270cfff9a90520b1d53
HongJunHyeok <edb1631@naver.com>
2021-06-07 19:22:16 +0900

    COMPLETE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <input type="text" v-model="input" placeholder="책 검색을 해보세요!" />
-    <button @click="getBookAPI">검색</button>
+    <div class="input_container">
+      <input type="text" v-model="input" placeholder="책 검색을 해보세요!" />
+      <button @click="getBookAPI">검색</button>
+    </div>
     <ul v-if="books.length !== 0">
       <li v-for="bookItem in books" v-bind:key="bookItem.isbn" class="BookItem">
         <h1>
@@ -10,21 +12,59 @@
         <img :src="bookItem.thumbnail" />
         <span>{{ bookItem.contents }}</span>
         <div class="meta">
-          <span>{{ bookItem.price }}</span>
+          <span>정가 : {{ bookItem.price }}원</span>
+          <strong>{{ bookItem.status }}</strong>
         </div>
       </li>
     </ul>
-    <div v-if="books.length === 0">위 검색창으로 책을 탐색해보세요!</div>
+    <h1 v-if="books.length === 0">위 검색창으로 책을 탐색해보세요</h1>
   </div>
 </template>
 
 <style scoped>
+.home {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.input_container {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.input_container input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  outline: none;
+  border: 3px solid gray;
+}
+.input_container input:focus {
+  box-sizing: border-box;
+  border: 3px solid #42b983;
+}
+
+.input_container button {
+  border: none;
+  width: 100%;
+  padding: 10px;
+  background: #42b983;
+  color: #2c3e50;
+  font-weight: bold;
+  cursor: pointer;
+  transition: ease 0.1s;
+}
+.input_container button:hover {
+  filter: brightness(80%);
+}
+
 ul {
   width: 800px;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+  padding-left: 0px;
 }
 .BookItem {
   width: 300px;
@@ -37,13 +77,16 @@ ul {
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   margin-bottom: 20px;
 }
-.BookItem h1 {
-}
-.BookItem span {
-}
 .BookItem img {
   width: 200px;
   height: 300px;
+}
+.BookItem .meta {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  background-color: lightgray;
+  text-align: center;
 }
 </style>
 
