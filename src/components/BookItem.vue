2329f800ccca37769175135976e7b1081701186e
HongJunHyeok <edb1631@naver.com>
2021-06-08 10:18:09 +0900

    ADD component props

---
+++ src/components/BookItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-bind="bookItem" class="BookItem">
+    <h1>
+      {{ bookItem.title }}
+    </h1>
+    <img :src="bookItem.thumbnail" />
+    <span>{{ bookItem.contents }}</span>
+    <div class="meta">
+      <span>정가 : {{ bookItem.price }}원</span>
+      <strong>{{ bookItem.status }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bookItem: Object,
+    default: {},
+  },
+};
+</script>
+
+<style scoped>
+.BookItem {
+  width: 300px;
+  padding: 20px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  margin-bottom: 20px;
+}
+.BookItem img {
+  width: 200px;
+  height: 300px;
+}
+.BookItem .meta {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  background-color: lightgray;
+  text-align: center;
+}
+.BookItem .meta span {
+  color: #42b983;
+  font-weight: bold;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,8 @@
       <input type="text" v-model="input" placeholder="책 검색을 해보세요!" />
       <button @click="getBookAPI">검색</button>
     </div>
-    <ul v-if="books.length !== 0">
-      <li v-for="bookItem in books" v-bind:key="bookItem.isbn" class="BookItem">
-        <h1>
-          {{ bookItem.title }}
-        </h1>
-        <img :src="bookItem.thumbnail" />
-        <span>{{ bookItem.contents }}</span>
-        <div class="meta">
-          <span>정가 : {{ bookItem.price }}원</span>
-          <strong>{{ bookItem.status }}</strong>
-        </div>
-      </li>
+    <ul v-for="bookItem in this.books" v-bind:key="bookItem.idsn">
+      <BookItem :bookItem="bookItem" />
     </ul>
     <h1 v-if="books.length === 0">위 검색창으로 책을 탐색해보세요</h1>
   </div>
@@ -65,32 +55,6 @@ ul {
   flex-wrap: wrap;
   justify-content: space-around;
   padding-left: 0px;
-}
-.BookItem {
-  width: 300px;
-  padding: 20px;
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  margin-bottom: 20px;
-}
-.BookItem img {
-  width: 200px;
-  height: 300px;
-}
-.BookItem .meta {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  background-color: lightgray;
-  text-align: center;
-}
-.BookItem .meta span {
-  color: #42b983;
-  font-weight: bold;
 }
 </style>
 
